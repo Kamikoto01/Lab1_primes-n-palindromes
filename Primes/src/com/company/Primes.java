0108f816186957e05666b84e4d4069d8eadd2a3d
Kamikoto01 <53916421+Kamikoto01@users.noreply.github.com>
2022-02-15 20:49:36 +0300

    Update Primes.java

--- Primes/src/com/company/Primes.java
+++ Primes/src/com/company/Primes.java
@@ -1,14 +1,15 @@
 package com.company;
 
 public class Primes {
-
+    // если число не делится на все числа от 2 до этого числа, то оно выводится в консоль
+    // Для красоты добавил sout'ы: будет выводится как матрица 5х5 с отступами
     public static void main(String[] args) {
         int count = 0;
         for (int i = 2; i < 101; i++) {
-            if(isPrime(i)) {
-                count++;
-                if(i < 10) System.out.print(" ");
-                System.out.print(i + " ");
+            if(isPrime(i)) { // вызов метода isPrime - проверка числа i
+                count++; 
+                if(i < 10) System.out.print(" "); 
+                System.out.print(i + " ");  
                 if(count % 5 == 0) System.out.println("");
 
             }
