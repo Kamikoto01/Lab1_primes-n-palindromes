1b314e5f8909a9360bdb9f543199e31fe2ffd7c7
Kamikoto01 <53916421+Kamikoto01@users.noreply.github.com>
2022-02-15 20:52:58 +0300

    Comments added

--- Palindromes/src/com/company/Palindromes.java
+++ Palindromes/src/com/company/Palindromes.java
@@ -15,12 +15,12 @@ public class Palindromes {
             }
         }
     }
-
+    // проверяем строку аргумент: полиндром или нет
     public static boolean isPalindrome(String str){
         str = str.toLowerCase(Locale.ROOT);
         return reverseString(str).equals(str);
     }
-
+    // метод переворачивает строку
     public static String reverseString(String str) {
         String result = "";
             for (int i = str.length() - 1; i > -1; i--) {
